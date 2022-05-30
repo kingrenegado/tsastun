4b5a65ec8b6ea108ad92eec52900119fefe79b71
Enrique Lara Guerrero <56370716+kingrenegado@users.noreply.github.com>
2022-05-30 17:06:05 -0500

    Valores

--- template/valores.php
+++ template/valores.php
@@ -1,29 +1,56 @@
-<div class="container-fluid  pt-5" name="mision" id="mision">
-        <div class="container py-5">
-            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h3 class="fw-bold text-uppercase color_morado">Misión, Visión</h3>
+<div class="container-fluid  pt-10" name="valores" id="valores">
+        <div class="container">
+            <div class="section-title text-center position-relative pb-3 mb-3 mx-auto" style="max-width: 600px;">
+                <h5 class="fw-bold color_morado text-uppercase">Por qué elegirnos</h5>
             </div>
             <div class="row g-5">
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <div class="service-item bg-light rounded d-flex flex-column">
-                        <h4 class="mb-3 correo">Misión</h4>
-                        <p class="m-0">Brindar la tecnología más innovadora a medida de las necesidades de nuestros
-                            clientes, con
-                            el objetivo de ayudar a incrementar su competitividad y productividad. Para ello
-                            implementamos y desarrollamos soluciones prácticas adaptadas a sus necesidades
-                            ofreciendo una solución global a empresas, profesionales, administraciones y usuarios
-                            particulares.</p>
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12">
+                            <div class="boton_contacto rounded d-flex align-items-center justify-content-center mb-3"
+                                style="width: 60px; height: 60px;">
+                                <i class="fa fa-cubes"></i>
+                            </div>
+                            <h4>Compromiso:</h4>
+                            <p class="mb-0">Concentramos nuestros esfuerzos en satisfacer las necesidades de
+                                nuestros grupos de interés.</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="boton_contacto rounded d-flex align-items-center justify-content-center mb-3"
+                                style="width: 60px; height: 60px;">
+                                <i class="fa fa-award"></i>
+                            </div>
+                            <h4>Innovación:</h4>
+                            <p class="mb-0">Mantenemos una actitud creativa para mejorar nuestros productos,
+                                procesos y servicios.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <div class="service-item bg-light rounded d-flex flex-column">
-                        <h4 class="mb-3 correo">Visión</h4>
-                        <p class="m-0">Ser la mejor opción del mercado en soluciones de Tecnologías de la Información
-                            para
-                            nuestros clientes, para ello abarcaremos todos los servicios que ofrecemos actualmente
-                            incrementando los que vayan surgiendo debido a la necesidad de cambio provocado por los
-                            avances tecnológicos. Esto es así, ya que somos una empresa en constante innovación ya
-                            que el sector de TI así lo requiere.</p>
+                <div class="col-lg-4" style="min-height: 350px;">
+                    <div class="position-relative h-100">
+                        <img class="position-absolute w-100 h-100 rounded " src="img/feature.jpg"
+                            style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="row g-5">
+                        <div class="col-12 ">
+                            <div class="boton_contacto rounded d-flex align-items-center justify-content-center mb-3"
+                                style="width: 60px; height: 60px;">
+                                <i class="fa fa-users-cog"></i>
+                            </div>
+                            <h4>Colaboración:</h4>
+                            <p class="mb-0">Hacemos sinergia con nuestros proveedores y clientes para mejorar día a
+                                día la calidad y así satisfacer sus necesidades.</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="boton_contacto rounded d-flex align-items-center justify-content-center mb-3"
+                                style="width: 60px; height: 60px;">
+                                <i class="fa fa-server "></i>
+                            </div>
+                            <h4>Transparencia</h4>
+                            <p class="mb-0">Actuamos congruentemente con los principios de ética empresarial.</p>
+                        </div>
                     </div>
                 </div>
             </div>
